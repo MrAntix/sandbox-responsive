396656cb93d1da010c2565b38be46d45b12e1cc4
Anthony Johnston <aj@antix.co.uk>
2013-08-25 20:20:31 +0100

    refactored to smaller and better code
    uses a large margin on the body of the page to force correct innerheight
    on mobile safari

--- Sandbox.Responsive.Web/Script/page.js
+++ Sandbox.Responsive.Web/Script/page.js
@@ -1,46 +1,29 @@
 ﻿
 /* page.js - for full height responsive app
-    with thanks, https://github.com/scottjehl/Hide-Address-Bar
 */
 (function(win) {
 
     var doc = win.document,
-        page = doc.getElementById("Page");
-
-    var setHeight = function() {
-        page.style.height = "150%";
-        win.setTimeout(function() {
+        page = doc.getElementById("Page"),
+        setHeight = function () {
+            if (!doc.body) return;
+            
+            doc.body.style.marginBottom = "1000px";
+            win.scrollTo(0, 1);
             page.style.height = (win.innerHeight ? win.innerHeight : doc.documentElement.clientHeight) + "px";
-        }, 0);
-    };
+            doc.body.style.marginBottom = "0";
 
-    if (win.addEventListener) {
-        var getScrollTop = function() {
-            return win.pageYOffset
-                || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop
-                || doc.body.scrollTop
-                || 0;
-            },
-            hideAddress = function() {
-
-                win.scrollTo(0, 1);
-                var scrollTop = 1,
-                    bodycheck = win.setInterval(function() {
-                        if (doc.body) {
-                            clearInterval(bodycheck);
-                            scrollTop = getScrollTop();
-                            win.scrollTo(0, scrollTop === 1 ? 0 : 1);
-                        }
-                    }, 15);
-            };
-
-        hideAddress();
-
-        win.addEventListener("resize", setHeight, false);
-        win.addEventListener("orientationchange", hideAddress, false);
-
-    } else win.attachEvent("onresize", setHeight);
-
+            var id = win.setInterval(function() {
+                if (doc.body) {
+                    clearInterval(id);
+                    win.scrollTo(0, 0);
+                }
+            }, 15);
+        };
+    
     setHeight();
+
+    if (win.addEventListener) win.addEventListener("resize", setHeight, false);
+    else win.attachEvent("onresize", setHeight);
 
 })(this);
